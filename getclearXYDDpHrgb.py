06a18127225076f88c9ce714ec9072b9c9dda287
EZreal-Map <tangkaiwen00@gmail.com>
2023-07-31 08:58:10 +0800

    getXYDpH.ply

--- getclearXYDDpHrgb.py
+++ getclearXYDDpHrgb.py
@@ -1,6 +1,5 @@
 import time
 import os
-import getColor
 import laspy
 import numpy as np
 
@@ -49,6 +48,33 @@ def save_XYDDpHRGB_lasdata(txt_file_path,las_file_path):
     out_las.write(las_file_path)
 
 
+def generate_color_gradient(num_segments, start_color, end_color):
+    # 获取颜色渐变数组
+    gradient_colors = [(255,255,255)]
+    for i in range(num_segments-1):
+        t = i / (num_segments - 1)
+        r = start_color[0] + (end_color[0] - start_color[0]) * t
+        g = start_color[1] + (end_color[1] - start_color[1]) * t
+        b = start_color[2] + (end_color[2] - start_color[2]) * t
+        gradient_colors.append([round(r), round(g), round(b)])  # 将颜色值为0-255的范围 round()四省五入取整
+
+    return gradient_colors
+
+
+def get_interval_index(H):
+    # 区间定义，第一个元素表示最小值，最后一个元素表示无穷大，其他值为各个区间的上限值
+    intervals = [0,0.01, 0.25, 0.50, 1.0, 1.50, 2.0, 2.5,
+                 3, 3.5, 4, 4.5, 5, 7.5, 10, float('inf')]
+
+    # 循环遍历所有区间，找到第一个H小于等于的区间，然后返回对应的索引
+    for i in range(len(intervals)):
+        if H <= intervals[i+1]:
+            return i
+
+    # 如果H小于所有区间的第一个值，则属于第一个区间
+    return 0
+
+
 # 主函数main()
 # 记录开始时间
 start_time = time.time()
@@ -69,15 +95,15 @@ def read_and_filter_data(XYD_list_lines, input_file, output_file, threshold, gra
             XYD_list = XYD_list_lines[i].rstrip('\n').split(',')
             try:
                 H = float(XYDpH_list[2])-float(XYD_list[2])
-                if H > threshold:
-                    index = getColor.get_interval_index(H)
+                if H >= threshold:
+                    index = get_interval_index(H)
                     rgbList = gradient_colors[index]
                     subsurface_point = lines[i].rstrip(
                         '\n') + ',' + ','.join(str(element) for element in rgbList) + '\n'
                     kept_data.append(subsurface_point)
-                    upsurface_point = XYD_list_lines[i].rstrip(
-                        '\n') + ',' + ','.join(str(element) for element in rgbList) + '\n'
-                    kept_data.append(upsurface_point)
+                    # upsurface_point = XYD_list_lines[i].rstrip(
+                    #     '\n') + ',' + ','.join(str(element) for element in rgbList) + '\n'
+                    # kept_data.append(upsurface_point)
                 else:
                     deleted_data += 1
             except ValueError:
@@ -87,7 +113,7 @@ def read_and_filter_data(XYD_list_lines, input_file, output_file, threshold, gra
     with open(output_file, 'w') as f:
         f.writelines(kept_data)
 
-    return deleted_data, len(kept_data)/2, kept_data
+    return deleted_data, len(kept_data), kept_data
 
 
 
@@ -99,15 +125,15 @@ xyd_file_path = os.path.join(
     directory_path, sorted_subdirectories[0], 'XYD.txt')
 with open(xyd_file_path, 'r') as f:
     XYD_list_lines = f.readlines()
-threshold = 0.05  # 阈值
+threshold = 0.00  # 阈值
 count = 1
 # 生成颜色渐变数组，从浅蓝色到深蓝色
-intervals = [0, 0.25, 0.50, 1.0, 1.50, 2.0, 2.5,
+intervals = [0, 0.01,0.25, 0.50, 1.0, 1.50, 2.0, 2.5,
              3, 3.5, 4, 4.5, 5, 7.5, 10, float('inf')]
 num_segments = len(intervals)-1
 start_color = [149, 208, 238]
 end_color = [10, 9, 145]
-gradient_colors = getColor.generate_color_gradient(
+gradient_colors = generate_color_gradient(
     num_segments, start_color, end_color)
 for directory in sorted_subdirectories[1:]:
     input_file = os.path.join(directory_path, directory, 'XYDpH.txt')
